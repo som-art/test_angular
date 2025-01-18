f8cf3510b18bcb86a9bce8a30c104acee3ffcf3c
som-art <dassomosree59@gmail.com>
2025-01-18 19:41:53 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'test_app';
+  message = 'This is a test message';
+}
